025b973792573c3294d800a12f344bcf2fcf79da
GrnDev1 <grndev@mail.ru>
2023-02-25 00:20:07 +0300

    10. Реализовать собственную структуру данных - HashMap [#1008]

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -16,7 +16,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     @Override
     public boolean put(K key, V value) {
-        int i = indexFor(hash(key));
+        int i = indexFor(hash(getHashcode(key)));
         boolean result = false;
         if (table[i] == null) {
             table[i] = new MapEntry<>(key, value);
@@ -30,10 +30,15 @@ public class SimpleMap<K, V> implements Map<K, V> {
         return result;
     }
 
-    private int hash(Object key) {
+    private int getHashcode(K key) {
+        int hashcode = key == null ? 0 : key.hashCode();
+        return hashcode;
+    }
+
+    private int hash(int hashCode) {
         int result = 0;
-        if (key != null) {
-            result = key.hashCode() ^ key.hashCode() >>> 16;
+        if (hashCode != 0) {
+            result = hashCode ^ hashCode >>> 16;
         }
         return result;
     }
@@ -44,16 +49,24 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     private void expand() {
         capacity *= 2;
-        table = Arrays.copyOf(table, capacity);
+        MapEntry<K, V>[] temp = new MapEntry[capacity];
+        for (MapEntry<K, V> i : table) {
+            if (i != null) {
+                int index = indexFor(hash(getHashcode(i.key)));
+                if (temp[index] == null) {
+                    temp[index] = new MapEntry<>(i.key, i.value);
+                }
+            }
+        }
+        table = temp;
     }
 
     @Override
     public V get(K key) {
         V result = null;
-        for (MapEntry<K, V> temp : table) {
-            if (temp != null && hash(key) == hash(temp.key) && Objects.equals(temp.key, key)) {
-                result = temp.value;
-            }
+        MapEntry<K, V> temp = table[indexFor(hash(getHashcode(key)))];
+        if (temp != null && hash(getHashcode(key)) == hash(getHashcode(temp.key)) && Objects.equals(temp.key, key)) {
+            result = temp.value;
         }
         return result;
     }
@@ -61,14 +74,13 @@ public class SimpleMap<K, V> implements Map<K, V> {
     @Override
     public boolean remove(K key) {
         boolean result = false;
-        for (int i = 0; i < table.length; i++) {
-            MapEntry<K, V> temp = table[i];
-            if (temp != null && hash(key) == hash(temp.key) && Objects.equals(temp.key, key)) {
-                table[i] = null;
-                result = true;
-                count--;
-                modCount++;
-            }
+        int index = indexFor(hash(getHashcode(key)));
+        MapEntry<K, V> temp = table[index];
+        if (temp != null && hash(getHashcode(key)) == hash(getHashcode(temp.key)) && Objects.equals(temp.key, key)) {
+            table[index] = null;
+            result = true;
+            count--;
+            modCount++;
         }
         return result;
     }
@@ -77,13 +89,16 @@ public class SimpleMap<K, V> implements Map<K, V> {
     public Iterator<K> iterator() {
         return new Iterator<K>() {
             int index;
-            int sum;
+            int counter;
             int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
+                }
+                while (counter < table.length && table[counter] == null) {
+                    counter++;
                 }
                 return index < count;
             }
@@ -93,14 +108,8 @@ public class SimpleMap<K, V> implements Map<K, V> {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                for (int temp = sum; ; temp++) {
-                    if (table[temp] != null) {
-                        sum = temp;
-                        index++;
-                        break;
-                    }
-                }
-                return table[sum++].key;
+                index++;
+                return table[counter++].key;
             }
         };
     }
