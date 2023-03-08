4d2740b10d2da5fcfdd00e00159f097ab1f5fd4e
GrnDev1 <grndev@mail.ru>
2023-03-08 20:48:32 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+
+    public static int[][] multiple(int size) {
+        int[][] array = new int[size][size];
+        for (byte i = 0; i < array.length; i++) {
+            for (byte j = 0; j < array.length; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            int[][] array = multiple(9);
+            for (int[] temp : array) {
+                for (int i : temp) {
+                    out.write((i + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
