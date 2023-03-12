eb7067a6afbf27312ee09b288ecc6d5f737e4eaa
GrnDev1 <grndev@mail.ru>
2023-03-12 21:55:28 +0300

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(target)) {
+            StringBuilder result = new StringBuilder();
+            boolean flag = false;
+            String added;
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                boolean startsWith = line.startsWith("5") || line.startsWith("4");
+                if (startsWith && !flag) {
+                    flag = true;
+                    added = line.split(" ")[1];
+                    result.append(added).append(";");
+                } else if (!startsWith && flag) {
+                    added = line.split(" ")[1];
+                    result.append(added).append(System.lineSeparator());
+                    flag = false;
+                }
+            }
+            out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
