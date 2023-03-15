6c93448c5ad0e645d8dc3bd56cd7f51821c60de1
GrnDev1 <grndev@mail.ru>
2023-03-15 23:35:34 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,11 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        search(Path.of("./"), new DuplicatesVisitor());
-    }
-
-    public static void search(Path root, DuplicatesVisitor visitor) throws IOException {
-        Files.walkFileTree(root, visitor);
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), visitor);
         visitor.getDublicates();
     }
 }
