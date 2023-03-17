af9047cd211c5b78a35a7ce9b0463a64cdceb872
GrnDev1 <grndev@mail.ru>
2023-03-17 23:34:33 +0300

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -2,27 +2,32 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    private void validate(String[] args) {
-        if (args.length < 3) {
-            throw new IllegalArgumentException("Add the missing arguments");
-        }
-        File file = new File(args[0].split("=", 2)[1]);
+    private void validate(ArgsName name) {
+        String directory = name.get("d");
+        String excluded = name.get("e");
+        String output = name.get("o");
+        File file = new File(directory);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
+        if (excluded.length() < 2 || !excluded.startsWith(".")) {
+            throw new IllegalArgumentException("Extension does not exist");
+        }
+        if (output.length() < 5 || !output.endsWith(".zip")) {
+            throw new IllegalArgumentException("Invalid Zip name");
+        }
     }
 
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File source : sources) {
-                zip.putNextEntry(new ZipEntry(source.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -42,20 +47,14 @@ public class Zip {
         }
     }
 
-    public List<File> getFileList(ArgsName name) throws IOException {
-        List<Path> pathList = Search.search(Path.of(name.get("d")), p -> !p.toFile().getName().endsWith(name.get("e")));
-        List<File> fileList = new ArrayList<>();
-        for (Path path : pathList) {
-            fileList.add(path.toFile());
-        }
-        return fileList;
-    }
-
     public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Add the missing arguments");
+        }
         Zip zip = new Zip();
-        zip.validate(args);
         ArgsName name = ArgsName.of(args);
-        List<File> fileList = zip.getFileList(name);
-        zip.packFiles(fileList, new File(name.get("o")));
+        zip.validate(name);
+        List<Path> pathList = Search.search(Path.of(name.get("d")), p -> !p.toFile().getName().endsWith(name.get("e")));
+        zip.packFiles(pathList, new File(name.get("o")));
     }
 }
