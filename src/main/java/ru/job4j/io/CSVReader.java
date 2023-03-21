df623be5d838896ca65aa48a55135687b534c07f
GrnDev1 <grndev@mail.ru>
2023-03-21 14:22:34 +0300

    17. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -5,12 +5,13 @@ import java.util.*;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) {
+        StringBuilder builder = new StringBuilder();
+        String out = argsName.get("out");
         String delimiter = argsName.get("delimiter");
         String[] filters = argsName.get("filter").split(",");
         Integer[] indexes = new Integer[filters.length];
         Map<String, Integer> map = new HashMap<>();
-        try (Scanner scanner = new Scanner(new FileReader(argsName.get("path")));
-             PrintWriter writer = new PrintWriter(argsName.get("out"))) {
+        try (Scanner scanner = new Scanner(new FileReader(argsName.get("path")))) {
             if (scanner.hasNextLine()) {
                 String[] headers = scanner.nextLine().split(delimiter);
                 for (int i = 0; i < headers.length; i++) {
@@ -20,24 +21,31 @@ public class CSVReader {
                     String temp = filters[j];
                     indexes[j] = map.get(temp);
                     if (j == filters.length - 1) {
-                        writer.print(temp);
+                        builder.append(temp);
                         break;
                     }
-                    writer.print(temp + delimiter);
+                    builder.append(temp + delimiter);
                 }
-                writer.println();
+                builder.append(System.lineSeparator());
             }
             while (scanner.hasNextLine()) {
                 String[] tempLine = scanner.nextLine().split(delimiter);
                 for (int i = 0; i < indexes.length; i++) {
                     String temp = tempLine[indexes[i]];
                     if (i == filters.length - 1) {
-                        writer.print(temp);
+                        builder.append(temp);
                         break;
                     }
-                    writer.print(temp + delimiter);
+                    builder.append(temp + delimiter);
                 }
-                writer.println();
+                builder.append(System.lineSeparator());
+            }
+            if ("stdout".equals(out)) {
+                System.out.println(builder);
+            } else {
+                try (PrintWriter writer = new PrintWriter(out)) {
+                    writer.println(builder);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -56,8 +64,8 @@ public class CSVReader {
         if (!";".equals(delimiter)) {
             throw new IllegalArgumentException("Invalid delimiter");
         }
-        if (output.length() < 5 || !output.endsWith(".csv")) {
-            throw new IllegalArgumentException("Invalid csv-file name");
+        if (!"stdout".equals(output) && (output.length() < 5 || !output.endsWith(".csv"))) {
+            throw new IllegalArgumentException("Invalid output name");
         }
         if ("".equals(filter)) {
             throw new IllegalArgumentException("Invalid filter");
